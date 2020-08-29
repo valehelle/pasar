5e52c34350d37ca2f27dc117dc11ff79e4ab001b
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2020-08-29 15:45:26 +0800

    change design

--- lib/kangarfc_web/controllers/page_controller.ex
+++ lib/kangarfc_web/controllers/page_controller.ex
@@ -5,7 +5,7 @@ defmodule KangarfcWeb.PageController do
 
   def index(conn, _params) do
     posts = Ads.list_posts()
-    render(conn, "index.html", posts: posts)
+    render(conn, "landing.html", posts: posts)
   end
 
   def show(conn, %{"post_id" => post_id}) do
