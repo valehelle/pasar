788a82d464816b4ec5c2e7299eda07db01d7402a
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2020-08-29 16:12:01 +0800

    change config

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :kangarfc, KangarfcWeb.Endpoint,
-  url: [host: "www.bazzarbear.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "http", host: "www.bazzarbear.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
